e2bbe44b150b7843a2ceb3e288d99419ecb4e322
brandonLee <brandonlee196582@gmail.com>
2023-04-05 13:24:32 -0600

    setup all get routes for api

--- ims-api/app-control/control.js
+++ ims-api/app-control/control.js
@@ -5,4 +5,21 @@ const getRoot = (req, res) => {
     res.status(200).send('server up')
 }
 
-module.exports = { knex, getRoot };
+const getData = (req, res, entity, id) => {
+  if (id) {
+    knex
+    .select('*')
+    .where({id: id})
+    .from(`${entity}_table`)
+    .then(data => res.status(200).send(data))
+  } else {
+    knex
+    .select('*')
+    .from(`${entity}_table`)
+    .then(data => res.status(200).send(data))
+  }
+}
+
+
+
+module.exports = { knex, getRoot, getData };
--- ims-api/index.js
+++ ims-api/index.js
@@ -4,17 +4,32 @@ const cors = require('cors');
 require("dotenv").config();
 const port = 3001;
 
-const { knex, getRoot } = require('./app-control/control')
+const { knex, getRoot, getData, getDataById } = require('./app-control/control')
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   getRoot(req, res);
 })
 
-app.get('/users', function (req, res) {
-  getUsers(req, res);
+// get all data from a passed in table
+app.get('/:entity', (req, res) => {
+  var { entity } = req.params;
+  entity === 'user' || entity === 'item' ? getData(req, res, `${entity}`) : res.send(`Unable to locate data for ${entity}.`)
+})
+
+// get an entity from a passed table by a passed id
+app.get('/:entity/:id', (req, res) => {
+  var { entity, id } = req.params;
+  entity === 'user' || entity === 'item' ? getData(req, res, `${entity}`, id) : res.send(`Unable to locate data for ${entity}/${id}.`)
+})
+
+// 
+app.post("/:entity", async (req, res) => {
+  var { entity } = req.params;
+  await req;
+  postUxo(req, res);
 })
 
 app.listen(port, () => {console.log(`server listening on port ${port}`)})
