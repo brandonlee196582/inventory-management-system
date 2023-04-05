141e10796748f675008de718f3d6731f0586b35e
brandonLee <brandonlee196582@gmail.com>
2023-04-05 11:11:27 -0600

    created db seed files

--- ims-api/seeds/01_users_seed.js
+++ ims-api/seeds/01_users_seed.js
@@ -1,13 +1,24 @@
+const bcrypt = require('bcryptjs');
+const saltRounds = 10;
+const seedPassword = 'password';
+var passHash = '';
+bcrypt.genSalt(saltRounds, (err, salt) => {
+  bcrypt.hash(seedPassword, salt, (err, hash) => {
+    passHash = hash;
+  })
+})
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex.schema.raw('TRUNCATE user CASCADE')
+  await knex('user').del()
+  await knex('user').insert([
+    {first_name: 'userFirst1', last_name: 'userLast1', username: 'username1', password: passHash},
+    {first_name: 'userFirst2', last_name: 'userLast1', username: 'username1', password: passHash},
+    {first_name: 'userFirst3', last_name: 'userLast1', username: 'username1', password: passHash}
   ]);
 };
